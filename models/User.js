64a0207ea975c5e54daf3acda7b530f426ebefcb
Andrés Muñoz <andresestebanmu@gmail.com>
2025-04-28 16:19:24 -0400

    mongo fix

--- models/User.js
+++ models/User.js
@@ -26,11 +26,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         match: [/^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,}$/gm],
         required: true
-    },
-    salt: String,
-    isAdmin: {
-        type: Boolean,
-        default: false
     }
     // favoriteProducts: {
     //     type: mongoose.Types.ObjectId,
